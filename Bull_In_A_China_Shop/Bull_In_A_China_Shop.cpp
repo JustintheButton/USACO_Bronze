9bf327cbb99b300c029e3c02c3997789da27c3e9
JUSTIN HUANG <huangjus@studentmail.conroeisd.net>
2021-06-27 11:28:44 -0500

    finished problem

--- Bull_In_A_China_Shop/Bull_In_A_China_Shop.cpp
+++ Bull_In_A_China_Shop/Bull_In_A_China_Shop.cpp
@@ -1,7 +1,98 @@
 #include <fstream>
 #include <iostream>
+#include <cmath>
 #include <vector>
 using namespace std;
+vector<vector<char>> subString(const vector<vector<char>> &piece) {
+    int maximumj = numeric_limits<int>::min();
+    int maximumi = numeric_limits<int>::min();
+    int minimumj = numeric_limits<int>::max();
+    int minimumi = numeric_limits<int>::max();
+    for (int i = 0; i < piece.size(); i++) {
+        for (int j = 0; j < piece[i].size(); j++) {
+            if (piece[i][j] == '#') {
+                if (maximumj < j) {
+                    maximumj = j;
+                }
+                if (minimumj > j) {
+                    minimumj = j;
+                }
+                if (maximumi < i) {
+                    maximumi = i;
+                }
+                if (minimumi > i) {
+                    minimumi = i;
+                }
+            }
+        }
+    }
+    vector<char>temp(maximumj - minimumj + 1);
+    vector<vector<char>> returnVector(maximumi-minimumi+1, temp);
+    for (int i = minimumi; i < maximumi+1; i++) {
+        for (int j = minimumj; j < maximumj + 1; j++) {
+            returnVector[i-minimumi][j-minimumj] = piece[i][j];
+        }
+    }
+    return returnVector;
+}
+bool singleMatch(
+    vector<vector<char>>& target,
+    const vector<vector<char>>& piece1)
+{
+    vector<vector<char>> compactString = subString(piece1);
+    bool foundMatch = false;
+    int matchingShiftx = -1;
+    int matchingShifty = -1;
+    for (int shiftx = 0; shiftx < target.size() - compactString.size() + 1; shiftx++) {
+        for (int shifty = 0; shifty < target[0].size() - compactString[0].size() + 1; shifty++) {
+            bool isRightShift = true;
+            for (int compactx = 0; compactx < compactString.size(); compactx++) {
+                for (int compacty = 0; compacty < compactString[compactx].size(); compacty++) {
+                    if (compactString[compactx][compacty] != target[compactx + shiftx][compacty + shifty] &&
+                        compactString[compactx][compacty] == '#') {
+                        isRightShift = false;
+                        break;
+                    }
+                }
+                if (isRightShift == false) {
+                    break;
+                }
+            }
+            if (isRightShift == true) {
+                foundMatch = true;
+                matchingShiftx = shiftx;
+                matchingShifty = shifty;
+                break;
+            }
+        }
+        if (foundMatch == true) {
+            break;
+        }
+    }
+    if (matchingShiftx != -1 && matchingShifty != -1) {
+        for (int i = 0; i < compactString.size(); i++) {
+            for (int j = 0; j < compactString[1].size(); j++) {
+                if(compactString[i][j] == '#')
+                    target[matchingShiftx + i][matchingShifty + j] = '.';
+            }
+        }
+    }
+
+    return foundMatch;
+}
+bool compare(
+    const vector<vector<char>> &target, 
+    const vector<vector<char>> &piece1, 
+    const vector<vector<char>> &piece2) 
+{
+    vector<vector<char>> copyTarget = target;
+    bool firstCheck = singleMatch(copyTarget, piece1);
+    if (!firstCheck) {
+        return false;
+    }
+    bool secondCheck = singleMatch(copyTarget, piece2);
+    return secondCheck;
+}
 int main()
 {
     ifstream infile;
@@ -27,28 +118,17 @@ int main()
             }
         }
     }
-    //for (int i = 0; i < k; i++) {
-    //    for (int j = 0; j < k; j++) {
-    //        if (isFilledPieces[i] + isFilledPieces[j] == originalFilled && i != j && i < j) {
-    //            outfile << i+1 << " " << j+1;
-    //        }
-    //    }
-    //}
+    bool current = false;
+    for (int ithpiece = 0; ithpiece < k; ithpiece++) {
+        for (int jthpiece = 0; jthpiece < k; jthpiece++) {
+            if (ithpiece >= jthpiece) {
+                continue;
+            }
+            current = compare(original, pieces[ithpiece], pieces[jthpiece]);
+            if (current == true) {
+                outfile << ithpiece + 1<< " " << jthpiece+1;
+            }
+        }
+    }
 
-    //for (int i = 0; i < n; i++) {
-    //    for (int j = 0; j < n; j++) {
-    //        cout << original[i][j];
-    //    }
-    //    cout << endl;
-    //}
-    //cout << endl;
-    //for (int i = 0; i < k; i++) {
-    //    for (int j = 0; j < n; j++) {
-    //        for (int k = 0; k < n; k++) {
-    //            cout << pieces[i][j][k];
-    //        }
-    //        cout << endl;
-    //    }
-    //    cout << endl;
-    //}
 }
